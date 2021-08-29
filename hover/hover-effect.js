9cfa0ffc7b3352757bb87f8b89104be9ab46ef1c
Sherinsedalo <sherin.sedalo@gmail.com>
2021-08-29 15:21:21 +0200

    adding landing page+scroll down

---
+++ hover/hover-effect.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('a[href*=#]').on('mouseover', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
+  
