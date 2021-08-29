bbc19f319d01d2100c366647a920db96ffd3e8a2
Sherinsedalo <sherin.sedalo@gmail.com>
2021-08-29 19:36:34 +0200

    Adding the first version of the project

--- hover/hover-effect.js
+++ hover/hover-effect.js
@@ -1,8 +1,23 @@
-$(function() {
-    $('a[href*=#]').on('mouseover', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
-  });
+$(function () {
+  var scrollButton = document.getElementById("scroll-button");
 
-  
+  scrollButton.addEventListener("mouseenter", function (event) {
+    event.preventDefault();
+    var elmntToView = document.getElementById("main-page");
+    elmntToView.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+//   $("a[href*=#]").on("mouseover", function (e) {
+//     e.preventDefault();
+//     var elmntToView = document.getElementById("main-page");
+//     elmntToView.scrollIntoView({behavior: "smooth"});
+//   //  var mainPageOffset = $("#main-page").offset().top;
+
+//   //   $("html, body").animate(
+//   //     { scrollTop: mainPageOffset },
+//   //     500,
+//   //     "linear"
+//   //   );
+//   });
+// });
